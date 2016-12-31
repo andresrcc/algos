5edb323dd21108c48d3b6ce62dc8c02650c9e26b
andresrcc <andrescordova87@gmail.com>
2016-12-31 16:53:31 -0500

    Fixed heaps

--- Heaps.cpp
+++ Heaps.cpp
@@ -1,59 +1,63 @@
 //Heaps
 
-vector<int> BubbleDown(vector<int> A, int i){
-  int l = 2 * i + 1;
-  int r = 2 * i + 2;
+void BubbleDown(vector<int>& A, int i)
+{
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
 
-  if(l >= A.size()) return A;
+    if (l >= A.size()) return;
 
-  int min_i = i;
+    int min_i = i;
 
-  if(A[l] < A[i]) min_i = l;
+    if (A[l] < A[i]) min_i = l;
 
-  if(r < A.size() && A[r] < A[min_i]) min_i = r;
+    if (r < A.size() && A[r] < A[min_i]) min_i = r;
 
-  if(min_i != i){
-    iter_swap(A.begin() + i, A.begin() + min_i);
-    A = BubbleDown(A,min_i);
-  }
-
-  return A;
+    if (min_i != i)
+    {
+        iter_swap(A.begin() + i, A.begin() + min_i);
+        BubbleDown(A, min_i);
+    }
 }
 
 //verify but im pretty sure this is ok
-int getParent(int i){
-  if(i%2 == 0) return (i - 2) / 2;
-  return (i - 1) / 2;
+int getParent(int i)
+{
+    if (i % 2 == 0) return (i - 2) / 2;
+    return (i - 1) / 2;
 }
 
 
-vector<int> BubbleUp(vector<int> A, int i){
-  if(i == 0) return A;
+void BubbleUp(vector<int>& A, int i)
+{
+    if (i == 0) return;
 
-  int parent_i = getParent(i);
+    int parent_i = getParent(i);
 
-  if(A[i] < A[parent_i]){
-    swap(A[i], A[parent_i]);
-    A = BubbleUp(A,parent_i);
-  }
-  return A;
+    if (A[i] < A[parent_i])
+    {
+        iter_swap(A.begin() + i, A.begin() + parent_i);
+        BubbleUp(A, parent_i);
+    }
 }
 
-vector<int> heapify(vector<int> A){
-  for(int i = A.size() - 1; i >=0; i--)
-    A = BubbleDown(A,i);
-  return A;
+void heapify(vector<int>& A)
+{
+    for (int i = A.size() - 1; i >= 0; i--)
+        BubbleDown(A, i);
 }
 
-vector<int> insertIntoHeap(vector<int> A, int d){
-  if(A.size() == 0) return A;
-  A.push_back(d);
-  return BubbleUp(A,A.size()-1);
+void insertIntoHeap(vector<int>& A, int d)
+{
+    if (A.size() == 0) return;
+    A.push_back(d);
+    BubbleUp(A, A.size() - 1);
 }
 
-vector<int> removeFromHeap(vector<int> A){
-  if(A.size() == 0) return A;
-  A[0] = A[A.size() -1]; //copy last node to first
-  A.pop_back(); //pop back to remove last node
-  return BubbleDown(A,0); //bubble down to adjust the tree after removing
+void removeFromHeap(vector<int>& A)
+{
+    if (A.size() == 0) return;
+    A[0] = A[A.size() - 1]; //copy last node to first
+    A.pop_back(); //pop back to remove last node
+    BubbleDown(A, 0); //bubble down to adjust the tree after removing
 }
