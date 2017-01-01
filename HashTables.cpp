388dddb79a1567a10efb9a2488feece83d4550a6
andresrcc <andrescordova87@gmail.com>
2017-01-01 18:00:48 -0500

    Adding Hashmap with probing
    Should test this one thoroughly as well

--- HashTables.cpp
+++ HashTables.cpp
@@ -20,11 +20,11 @@ struct Node
 };
 
 
-class HashMap
+class HashMapChaining
 {
 	vector<Node*> hash_table;
 public:
-	HashMap()
+	HashMapChaining()
 	{
 		hash_table.assign(TABLE_SIZE, nullptr);
 	}
@@ -102,9 +102,91 @@ public:
 		}
 	}
 
-	~HashMap()
+	~HashMapChaining()
 	{
 		hash_table.clear();
 	}
 
 };
+
+//Linear Probing
+
+struct Entry
+{
+	int key;
+	int value;
+
+	Entry(int key, int value)
+	{
+		this->key = key;
+		this->value = value;
+	}
+};
+
+
+class HashMapProbing
+{
+	vector<Entry*> hash_table;
+public:
+	HashMapProbing()
+	{
+		hash_table.assign(TABLE_SIZE, nullptr);
+	}
+
+
+	int hash_function(int key)
+	{
+		return key % TABLE_SIZE;
+	}
+
+
+	void insert(int key, int value)
+	{
+		int hash = hash_function(key);
+
+		while (hash_table[hash] != nullptr && hash_table[hash]->key != key)
+		{
+			hash = hash_function(hash + 1);
+		}
+
+		if (hash_table[hash] != nullptr)
+			delete hash_table[hash];
+
+		hash_table[hash] = new Entry(key, value);
+	}
+
+	int search(int key)
+	{
+		int hash = hash_function(key);
+		while (hash_table[hash] != nullptr && hash_table[hash]->key != key)
+		{
+			hash = hash_function(hash + 1);
+		}
+
+		return hash_table[hash] == nullptr ? -1 : hash_table[hash]->value;
+	}
+
+
+	void remove(int key)
+	{
+		int hash = hash_function(key);
+
+		while (hash_table[hash] != nullptr)
+		{
+			if (hash_table[hash]->key == key)
+				break;
+
+			hash = hash_function(hash + 1);
+		}
+
+		if (hash_table[hash] == nullptr) return;
+
+		delete hash_table[hash];
+	}
+
+	~HashMapProbing()
+	{
+		hash_table.clear();
+	}
+};
+
